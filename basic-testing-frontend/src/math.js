95cd81ed2c78098066d2023db7884fc506dfa47a
Deepak Guptha <vzan2012@gmail.com>
2022-05-22 20:19:09 +0530

    refactor: updated functions for app

--- basic-testing-frontend/src/math.js
+++ basic-testing-frontend/src/math.js
@@ -1,3 +1,4 @@
+import { cleanNumbers } from "./util/numbers.js";
 export function add(numbers) {
   let sum = 0;
 
@@ -5,4 +6,15 @@ export function add(numbers) {
     sum += +number;
   }
   return sum;
+}
+
+export function calculateResult(numberValues) {
+  let result = "";
+  try {
+    const numbers = cleanNumbers(numberValues);
+    result = add(numbers).toString();
+  } catch (error) {
+    result = error.message;
+  }
+  return result;
 }
