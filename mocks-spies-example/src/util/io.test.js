a861c13312cea6c6882ddc6453573cc14dfdf343
Deepak Guptha <vzan2012@gmail.com>
2022-06-05 15:57:04 +0530

    feat(test): Added auto mock for fs module

--- mocks-spies-example/src/util/io.test.js
+++ mocks-spies-example/src/util/io.test.js
@@ -26,4 +26,16 @@ describe.concurrent("IO Tests", () => {
     // expect(fs.writeFile).toBeCalled();
     expect(fs.writeFile).toBeCalledWith(testFilename, testData);
   });
+  it("should returns a promise that resolves to no value if called uncorrectly", () => {
+    const testData = "This is test content";
+    const testFilename = "test.txt";
+
+    writeData(testData, testFilename);
+
+    // writeData - Promise returns void
+    expect(writeData(testData, testFilename)).resolves.toBeUndefined();
+
+    // expect(fs.writeFile).toBeCalled();
+    // expect(fs.writeFile).toBeCalledWith(testFilename, testData);
+  });
 });
