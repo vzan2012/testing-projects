7b24487ecbff8b4104d6c600aa3c121fb9617949
Deepak Guptha <vzan2012@gmail.com>
2022-06-06 15:36:13 +0530

    feat(test): updation of first test case for dom

--- testing-frontend/util/dom.test.js
+++ testing-frontend/util/dom.test.js
@@ -1,7 +1,7 @@
 import fs from "fs";
 import path from "path";
 
-import { it, vi } from "vitest";
+import { expect, it, vi } from "vitest";
 import { showError } from "./dom";
 
 const htmlDocPath = path.join(process.cwd(), "index.html");
@@ -10,8 +10,15 @@ const htmlDocumentContent = fs.readFileSync(htmlDocPath).toString();
 const window = new Window();
 const document = window.document;
 document.write(htmlDocumentContent);
+
+// Mock the Global - document
 vi.stubGlobal("document", document);
 
-it("Initial test", () => {
+it("should add an error paragraph into id=errors element", () => {
   showError("test message");
+
+  const errorElement = document.getElementById("errors");
+  const errorParagraph = errorElement.firstElementChild;
+
+  expect(errorParagraph).not.toBeNull();
 });
